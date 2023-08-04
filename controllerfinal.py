607b1b243d8b7fa16d5c3bf6928967356bffa984
Swastik Aryal <swastik_aryal@outlook.com>
2023-08-04 17:32:12 +0545

    double click added

--- controllerfinal.py
+++ controllerfinal.py
@@ -134,6 +134,7 @@ with mp_hands.Hands(min_detection_confidence = 0.5 , min_tracking_confidence = 0
                     
                     
                     
+                    
 
                     
 
